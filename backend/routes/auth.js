d2abfdfc41972e9af989429531e077a25d125587
dimension <dimension2414@gmail.com>
2021-11-01 23:00:44 +0530

    added express validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router(); 
 
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res)=>{ 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name','Please enter a valid name').isLength({ min: 3}),
+    body('email', 'The email is invalid').isEmail(),
+    body('password','Lol password').isLength({ min: 5 })
+],(req, res)=> {
+ const errors = validationResult(req);
+if (!errors.isEmpty()) {
+  return res.status(400).json({ errors: errors.array() });
+}
+User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).then(user => res.json(user))
+  .catch(err=>{console.log(err)
+res.json({error:"please enter a unique value"})})
+
+    
 } )
 
 module.exports = router
