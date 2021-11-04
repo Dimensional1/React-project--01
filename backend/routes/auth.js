72d76588099a5de6b664929f3a5445b300cb8527
dimension <dimension2414@gmail.com>
2021-11-04 22:15:26 +0530

    collections in thunder client 0.1

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,23 +4,33 @@ const { body, validationResult } = require('express-validator');
 const router = express.Router(); 
 
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', [
+// Create a User using: POST "/api/auth/createUser". Doesn't require login 
+router.post('/createUser ', [
     body('name','Please enter a valid name').isLength({ min: 3}),
     body('email', 'The email is invalid').isEmail(),
     body('password','Lol password').isLength({ min: 5 })
-],(req, res)=> {
+],async(req, res)=>{
+
+  // if there is error return bad request
+
  const errors = validationResult(req);
 if (!errors.isEmpty()) {
   return res.status(400).json({ errors: errors.array() });
 }
-User.create({
+// check wheather the user exist with email exist already
+
+let user =User.findOne({emial:req.body.email}) 
+if(user){
+  return res.status(400).json({error: "Sorry user with this email exist already "})
+}
+user= await User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-  }).then(user => res.json(user))
-  .catch(err=>{console.log(err)
-res.json({error:"please enter a unique value you repeating the same value"})})
+  })
+//   .then(user => res.json(user))
+//   .catch(err=>{console.log(err)
+// res.json({error:"please enter a unique value you repeating the same value"})})
 
     
 } )
